8a5cbd5acdb006e8a846fc82f0c7dc4a4bc19ef3
Brandon Wong <brandon@yerdle.com>
2021-08-31 17:26:17 -0400

    Add ynab client

---
+++ src/ynab/client/ynab.py
@@ -0,0 +1,45 @@
+import json
+import requests
+import logging
+
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+
+
+class YNABClient:
+    def __init__(self, token):
+        self.base_url = 'https://api.youneedabudget.com/v1'
+        self.headers = { 'Authorization': f'Bearer {token}', 'Content-Type': 'application/json' }
+
+
+    def get_budgets(self):
+        path = f'/budgets'
+        try:
+            res = requests.get(self.base_url + path, headers=self.headers)
+            return res.json()
+        except Exception as e:
+            log.error('get_budgets error. %s', e)
+            raise e
+
+
+    def get_transactions(self, budget_id, since_date):
+        path = f'/budgets/{budget_id}/transactions'
+        params = { 'since_date': since_date }
+        try:
+            res = requests.get(self.base_url + path, params=params, headers=self.headers)
+            return res.json()
+        except Exception as e:
+            log.error('get_transactions error. %s', e)
+            raise e
+
+
+    def update_transactions(self, budget_id, transactions):
+        data = json.dumps({ 'transactions': transactions })
+        path = f'/budgets/{budget_id}/transactions'
+        try:
+            res = requests.patch(self.base_url + path, data=data, headers=self.headers)
+            return res.json()
+        except Exception as e:
+            log.error('update_transactions error. %s', e)
+            raise e
